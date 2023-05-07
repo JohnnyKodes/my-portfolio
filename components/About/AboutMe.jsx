d015c9c92a1f9824b8cf79c7cc59ba49c3491517
JohnnyKodes <johnnykaram@protonmail.com>
2023-05-07 17:54:52 +0300

    Updated About Me text

--- components/About/AboutMe.jsx
+++ components/About/AboutMe.jsx
@@ -5,25 +5,29 @@ const AboutMe = () => {
     <div className="flex flex-col sm:w-[900px] max-w-full gap-6 mt-10 mb-14 text-justify m-auto">
       <h2 className="text-4xl font-bold">About</h2>
       <p className="text-2xl">
-        Hey, I'm Johnny. I'm a fresh computer science graduate eager to leave my
-        mark on the world of tech.
+        Hey there, I'm Johnny. Let me tell you a bit about myself.
       </p>
       <p className="text-lg text-zinc-400">
-        I've got a lot of nicknames: a software engineer, front-end developer,
-        UI/UX designer, amateur weight lifter, basketball prodigy, and my mom
-        calls me handsome.
+        I fell in love with coding at a young age, but it wasn't until I started
+        tinkering with various languages such as C++, javascript, and python
+        that I truly became enamored with the possibilities of software
+        development. From there, I've been on a quest to push the boundaries of
+        what's possible with code.
       </p>
       <p className="text-lg text-zinc-400">
-        Defining myself to the work I've done is never an action I follow. I
-        only define myself with the work I want to do. Skills can be taught but
-        personality is inherent. I strive to keep on learning, and always be
-        ahead of the curve especially in the exponentially fast world of tech.
+        As a software engineer, I've learned how valuable teamwork,
+        communication, and attention to detail really are in delivering
+        high-quality software products. Working collaboratively with my
+        colleagues has allowed me to grow as a developer, and I'm grateful for
+        the mentorship and guidance I've received.
       </p>
       <p className="text-lg text-zinc-400">
-        My boundless energy fuels me to follow numerous hobbies, interests, and
-        topics of study. I'm a fast learner constantly pushing myself to gain
-        new skills, start and juggle different projects while keeping my cool
-        through all the work.
+        My goal is to make a lasting impact on the software engineering world. I
+        believe that software has the potential to transform industries and
+        improve people's lives, and I'm excited to be a part of that journey.
+        I'm constantly exploring new technologies, learning new programming
+        languages, and experimenting with new approaches to software
+        development.
       </p>
     </div>
   );
